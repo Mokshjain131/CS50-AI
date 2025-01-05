9316e7511bdd2ca6524136ae4eb84495137c1ce1
Mokshjain131 <mokshjain131@gmail.com>
2025-01-05 22:44:01 +0530

    Update puzzle.py

--- Lec 1 - Knowledge/knights/puzzle.py
+++ Lec 1 - Knowledge/knights/puzzle.py
@@ -9,25 +9,62 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+initial_knowledge = And(
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave))
+)
+
+A_statement0 = And(AKnight, AKnave)
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    initial_knowledge,
+    Implication(A_statement0, AKnight),
+    Implication(Not(A_statement0), AKnave)
+)
+
+A_statement1 = And(
+    AKnave, BKnave
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    initial_knowledge,
+    Implication(A_statement1, AKnight),
+    Implication(Not(A_statement1), AKnave)
+)
+
+A_statement2 = And(
+    Or(And(AKnight, BKnight), And(AKnave, BKnave)), Not(And(And(AKnight, BKnight), And(AKnave, BKnave)))
+)
+
+B_statement2 = And(
+    Or(And(AKnight, BKnave), And(AKnave, BKnight)), Not(And(And(AKnight, BKnave), And(AKnave, BKnight)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    initial_knowledge,
+    Implication(A_statement2, AKnight),
+    Implication(Not(A_statement2), AKnave),
+    Implication(B_statement2, BKnight),
+    Implication(Not(B_statement2), BKnave)
 )
+
+A_statement3 = Or(AKnight, AKnave)
+
+B_statement3 = And(AKnave, CKnave)
+
+C_statement3 = AKnight
 
 # Puzzle 3
 # A says either "I am a knight." or "I am a knave.", but you don't know which.
@@ -35,7 +72,13 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    initial_knowledge,
+    Implication(A_statement3, AKnight),
+    Implication(Not(A_statement3), AKnave),
+    Implication(B_statement3, BKnight),
+    Implication(Not(B_statement3), BKnave),
+    Implication(C_statement3, CKnight),
+    Implication(Not(C_statement3), CKnave)
 )
 
 
